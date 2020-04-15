5a929b5c6300f3e245f1e634302ba90491f9aaf3
chemaguiniga2 <A01376669@itesm.mx>
2020-04-15 17:00:02 -0500

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:componentes/src/pages/home_page.dart';
-import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -10,7 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      //home: HomePage()
+      initialRoute: '/',
+      routes: getApplicationRoutes(),
+      onGenerateRoute: ( settings ) {
+        return MaterialPageRoute(
+          builder: (BuildContext context) => AlertPage()
+        );
+      },
     );
   }
 }
