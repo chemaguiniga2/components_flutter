d512a9d8cbdba6e1bfd58c409073c9e6978b577a
chemaguiniga2 <A01376669@itesm.mx>
2020-07-01 20:33:02 -0500

    List View

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -8,6 +8,7 @@ class SliderPage extends StatefulWidget {
 class _SliderPageState extends State<SliderPage> {
 
   double _valorSlider = 100.0; 
+  bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +21,11 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: <Widget>[
             _crearSlider(),
-            _crearImagen(),
+            _checkBox(),
+            _crearSwitch(),
+            Expanded(
+              child: _crearImagen(),
+            ),
           ],
         ),
       ),
@@ -36,10 +41,42 @@ class _SliderPageState extends State<SliderPage> {
       value: _valorSlider,
       min: 10.0,
       max: 100.0,
-      onChanged: ( valor ){
-
+      onChanged: (_bloquearCheck) ? null : ( valor ){
         setState(() {
           _valorSlider = valor;
+        });
+      },
+    );
+  }
+
+  Widget _checkBox() {
+    // return Checkbox(
+    //   value: _bloquearCheck,
+    //   onChanged: (valor){
+    //     setState(() {
+    //       _bloquearCheck = valor;
+    //     });
+    //   },
+    // );
+
+    return CheckboxListTile(
+      title: Text('Bloquear Slider'),
+      value: _bloquearCheck,
+      onChanged: (valor){
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+      title: Text('Bloquear Slider'),
+      value: _bloquearCheck,
+      onChanged: (valor){
+        setState(() {
+          _bloquearCheck = valor;
         });
       },
     );
